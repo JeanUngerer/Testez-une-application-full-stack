d2a3591a9f94015bd061f16cc0bb28ae8972202c
Jean Ungerer <j.ungerer@technologyandstrategy.com>
2024-04-03 17:46:12 +0200

    Add unit tests for user service and authentication
    This commit introduces unit tests for user service operations such as delete, find by ID and user authentication. It also modifies the application properties to change username and password credentials and includes a new application test file specifying the JPA and database properties for the test environment. The necessary dependencies for testing are added as well.

---
+++ back/src/test/java/com/openclassrooms/starterjwt/auth/AuthControllerTest.java
@@ -0,0 +1,81 @@
+package com.openclassrooms.starterjwt.auth;
+
+import com.openclassrooms.starterjwt.models.User;
+import com.openclassrooms.starterjwt.payload.request.LoginRequest;
+import com.openclassrooms.starterjwt.payload.request.SignupRequest;
+import com.openclassrooms.starterjwt.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static com.openclassrooms.starterjwt.utils.ObjectAsJsonStrings.asJsonString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc(addFilters = true)
+public class AuthControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Test
+    void registerUser() throws Exception {
+        String url = "/api/auth/register";
+
+        SignupRequest signupRequest = new SignupRequest();
+        signupRequest.setEmail("testNewUser@example.com");
+        signupRequest.setPassword("password123");
+        signupRequest.setFirstName("Jane");
+        signupRequest.setLastName("Smith");
+
+
+        MvcResult result = this.mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(signupRequest)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.message").value("User registered successfully!"))
+                .andReturn();
+    }
+
+    @Test
+    void loginUser() throws Exception {
+        String url = "/api/auth/login";
+
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setEmail("testUser@example.com");
+        loginRequest.setPassword("password123");
+
+        userRepository.save(User.builder()
+                .email("testUser@example.com")
+                .password(passwordEncoder.encode("password123"))
+                .firstName("John")
+                .lastName("Doe")
+                .build());
+
+        MvcResult result = this.mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(loginRequest)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.token").exists())
+                .andReturn();
+    }
+}
