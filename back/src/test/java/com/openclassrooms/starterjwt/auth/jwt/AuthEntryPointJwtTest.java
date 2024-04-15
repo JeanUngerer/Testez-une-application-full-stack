6184a07340522cf03f14f3736cb93aace41022f6
Jean Ungerer <j.ungerer@technologyandstrategy.com>
2024-04-15 16:34:49 +0200

    Add AuthEntryPointJwt and JwtUtils unit tests
    Added two new test classes, AuthEntryPointJwtTest and JwtUtilsTests, to enhance test coverage for JWT authentication.

---
+++ back/src/test/java/com/openclassrooms/starterjwt/auth/jwt/AuthEntryPointJwtTest.java
@@ -0,0 +1,49 @@
+package com.openclassrooms.starterjwt.auth.jwt;
+
+import com.openclassrooms.starterjwt.security.jwt.AuthEntryPointJwt;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.authentication.CredentialsExpiredException;
+import org.springframework.test.context.ActiveProfiles;
+
+import javax.servlet.http.HttpServletResponse;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc(addFilters = false)
+public class AuthEntryPointJwtTest {
+    @InjectMocks
+    private AuthEntryPointJwt authEntryPointJwt;
+
+    private MockHttpServletRequest request;
+    private MockHttpServletResponse response;
+    private CredentialsExpiredException authException;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.openMocks(this);
+        request = new MockHttpServletRequest();
+        response = new MockHttpServletResponse();
+        authException = new CredentialsExpiredException("TEST");
+    }
+
+    @Test
+    public void testCommence() throws Exception {
+        authEntryPointJwt.commence(request, response, authException);
+
+        assertEquals(HttpServletResponse.SC_UNAUTHORIZED, response.getStatus());
+        assertEquals("application/json", response.getContentType());
+        assertEquals("{\"path\":\"\",\"error\":\"Unauthorized\",\"message\":\"TEST\",\"status\":401}",
+          response.getContentAsString());
+    }
+
+
+}
